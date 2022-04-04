a28189646006987e9a47a33ff9f337759d28388f
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-04-04 12:03:51 +0530

    added game over panel when any player wins, restarting game when player clicks on reset button

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class ScoreManager : MonoBehaviour
     PlayerController player1;
     PlayerController player2;
     BallController ball;
+    public GameObject resetPanel;
 
     private void Start()
     {
@@ -19,12 +21,20 @@ public class ScoreManager : MonoBehaviour
         player2 = GameObject.Find("Player 2").GetComponent<PlayerController>();
         ball = GameObject.Find("Ball").GetComponent<BallController>();
     }
-
+    private void Update()
+    {
+        
+    }
     public void Score1Calculator(int scoreValue1)
     {
         score1 = score1 + scoreValue1;
         score1Text.text = score1.ToString();
         ResetGame();
+        if(score1 == 5)
+        {
+            resetPanel.SetActive(true);
+            Debug.Log("Player 2 WON");
+        }
     }
 
     public void Score2Calculator(int scoreValue2)
@@ -32,13 +42,22 @@ public class ScoreManager : MonoBehaviour
         score2 = score2 + scoreValue2;
         score2Text.text = score2.ToString();
         ResetGame();
+        if (score2 == 5)
+        {
+            resetPanel.SetActive(true);
+            Debug.Log("Player 1 WON");
+        }
+    }
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void ResetGame()
     {
         ball.RestartPosition();
-        player1.RestartGame();
-        player2.RestartGame();
+        //player1.RestartGame();
+        //player2.RestartGame();
     }
 
 }
